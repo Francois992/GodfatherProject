7bd8af0962150bb99d6379c64bee95395966943a
David <synlike@gmail.com>
2020-09-02 18:50:38 +0200

    Mini jeu 1  V1

--- Assets/Scripts/DactyloManager.cs
+++ Assets/Scripts/DactyloManager.cs
@@ -8,6 +8,8 @@ public class DactyloManager : MonoBehaviour
     [SerializeField]
     private string[] wordList;
     private string wordToCopy;
+    private int wordPosition; //Position dans le mot à recopier
+    private bool win;
 
     [SerializeField]
     private GameObject LetterPrefab;
@@ -20,33 +22,40 @@ public class DactyloManager : MonoBehaviour
     [SerializeField]
     private Text currentSuccessText;
 
-    private bool inputFocused = true;
+    private List<Text> letters = new List<Text>();
+
 
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        // Add Hide Input Caret
         InitNewWord();
-        Debug.Log(wordList[0]);
     }
 
     void Update()
     {
-        // Event qui appelle TransformToUpperCase à chaque changement de valeur
-        inputF.onValueChanged.AddListener(delegate { TransformToUpperCase(); });
+        // Event qui appelle OnInputValueChange à chaque changement de valeur
+        inputF.onValueChanged.AddListener(delegate { OnInputValueChange(); });
 
-        // Event qui appelle CheckIfValid à chaque changement de valeur
-        inputF.onValueChanged.AddListener(delegate { CheckIfValid(); });
+        if (Input.anyKey && !win && inputF.text != "")
+        {
+            CheckIfValid();
+        }
 
     }
 
     void InitNewWord()
     {
+        EmptyPreviousLetters();
+
         inputF.text = "";
         inputF.Select();
         inputF.ActivateInputField();
 
-        // Retourne chiffre aléatoire entre 0 et wordlist.length
+        wordPosition = 0;
+
+        // Retourne chiffre aléatoire entre 0 et wordlist.length => Améliorer le Random.Range si possible
         int value = Random.Range(0, wordList.Length - 1);
         wordToCopy = wordList[value].ToUpper();
 
@@ -56,18 +65,33 @@ public class DactyloManager : MonoBehaviour
 
     }
 
+    void EmptyPreviousLetters()
+    {
+        foreach(Transform child in GameObject.Find("WordToCopy").transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
     void InstantiateLetters()
     {
+        letters.Clear();
+
         for(int i = 0; i < wordToCopy.Length; i++)
         {
-            // Change 130 par détermination par le code
-            GameObject tmp = Instantiate(LetterPrefab, new Vector3(0 + (i * 130), 0, 0), Quaternion.identity) as GameObject;
+            GameObject tmp = Instantiate(LetterPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
 
             tmp.transform.SetParent(GameObject.Find("WordToCopy").transform, false);
             tmp.GetComponentInChildren<Text>().text = wordToCopy[i].ToString();
-           
+
+            letters.Add(tmp.GetComponentInChildren<Text>());
         }
 
+    }
+
+    void OnInputValueChange()
+    {
+        TransformToUpperCase();
     }
 
     // Fonction qui passe le texte de l'input en majuscule a chaque changement
@@ -76,16 +100,43 @@ public class DactyloManager : MonoBehaviour
         inputF.text = inputF.text.ToUpper();
     }
 
-    // Ajoute un au nombre de succès
-    void AddSuccess()
-    {
-        currentSuccess++;
-        currentSuccessText.text = currentSuccess.ToString();
-    }
-
     void CheckIfValid()
     {
+        if (inputF.text == wordToCopy[wordPosition].ToString()) 
+        {
+            letters[wordPosition].text = "";
+            wordPosition++;
+            Debug.Log("OK");
 
+            if(wordPosition == wordToCopy.Length)
+            {
+                AddSuccess();
+                if (currentSuccess < 3)
+                {
+                    InitNewWord();
+                }
+                else
+                {
+                    win = true;
+                    // Passer à la suite
+                    Debug.Log("C'est Win");
+                }
+            }
+        }
+        else
+        {
+            // Faire trembler écran
+            // Augmenter jauge de sel
+
+            foreach (Transform child in GameObject.Find("WordToCopy").transform)
+            {
+                // Do Something to all letters
+            }
+
+            Debug.Log("NOPE");
+        }
+
+        inputF.text = "";
 
         /*
         if(inputF.text == wordToCopy && currentSuccess < 3)
@@ -106,5 +157,24 @@ public class DactyloManager : MonoBehaviour
             }
         }
         */
+    }
+
+    string getCurrentProgression(int length)
+    {
+        string res = "";
+        for(int i = 0; i < length; i++)
+        {
+            res += wordToCopy[i].ToString();
+        }
+
+        Debug.Log(res);
+        return res;
+    }
+
+    // Ajoute un au nombre de succès
+    void AddSuccess()
+    {
+        currentSuccess++;
+        currentSuccessText.text = currentSuccess.ToString();
     }
 }
