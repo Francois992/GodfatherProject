a446f40c30a7ab6a855f0b0baa467772c4a5fc52
David <synlike@gmail.com>
2020-09-03 18:48:33 +0200

    Pause Menu Improvements

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class MenuFunctions : MonoBehaviour
 {
+    [SerializeField]
+    private GameManager gameManager;
+    
+    public void Restart()
+    {
+        gameManager.Restart();
+    }
+
     public void Exit()
     {
-        Application.Quit();
+        gameManager.ExitGame();
     }
 }
