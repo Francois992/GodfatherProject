5c8699a8318e7fdcf118b4b2bbf992495a6b7594
François <dessartsfran@gmail.com>
2020-09-04 12:20:30 +0200

    Add TitleScreen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    [SerializeField] private Canvas StoryScreen = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StoryScreen.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.SetActive(false);
+            StoryScreen.gameObject.SetActive(true);
+        }
+    }
+}
